2c0722b4a07cd0f36995ea9be3a56b9f20a7e3a2
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2017-09-15 16:58:52 +0900

    GensokyoDateTime.csの追加

---
+++ GensokyoDateTime/GensokyoDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crowolf.GensokyoDateTime
+{
+	/// <summary>
+	/// 幻想郷の暦、時刻として表現される瞬間を表します。
+	/// </summary>
+	public class GensokyoDateTime
+	{
+
+	}
+}
